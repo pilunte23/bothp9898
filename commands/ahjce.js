10a45990dfe664ba80ff868c388c41cddecf6bda
Pilunte <vad23@msn.com>
2018-05-09 19:00:52 +0200

    fix url

--- commands/ahjce.js
+++ commands/ahjce.js
@@ -9,16 +9,16 @@ module.exports = class AHjce extends Command {
     static action (message){
         let args = message.content.split(' ')
         args.shift()      
-        
+        let linkUrl
         if (!isNaN(args[0])){ 
-            let url = 'http://www.ahjce.fr/IMAGES/CARTES/AH-'+args[0]+'.jpg'                     
+            linkUrl = 'http://www.ahjce.fr/IMAGES/CARTES/AH-'+args[0]+'.jpg'                     
         }   
         else
         {      
-            let url = 'http://www.ahjce.fr/carte_liste.php?rech=' +args.join('%20')                 
+            linkUrl = 'http://www.ahjce.fr/carte_liste.php?rech=' +args.join('%20')                 
         }
         const https = require('https');
-        https.get(url, (resp) => {
+        https.get(linkUrl, (resp) => {
         let data = '';
         // A chunk of data has been recieved.
         resp.on('data', (chunk) => {
