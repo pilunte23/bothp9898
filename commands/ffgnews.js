34c74ef593317836091baac5ec195e9f5a3e014e
David Violeau <david.violeau_ssii@i-bp.fr>
2018-05-11 09:38:45 +0200

    Auto stash before merge of "master" and "origin/master"

--- commands/ffgnews.js
+++ commands/ffgnews.js
@@ -11,7 +11,7 @@ module.exports = class FFGNews extends Command {
     static action (message){
     console.log("action ffgnews")
         
-        JSDOM.fromURL("http://www.fantasyflightgames.fr/recherche/jeux/horreur_a_arkham_lcg", options).then(dom => {
+        JSDOM.fromURL("http://www.fantasyflightgames.fr/recherche/jeux/horreur_a_arkham_lcg").then(dom => {
             console.log(dom.serialize());
             });
         
