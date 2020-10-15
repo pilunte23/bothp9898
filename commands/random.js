a588a1c22c86accd44735ef02bdade744822c1cb
pilunte23 <vad23@msn.com>
2020-10-15 18:36:20 +0200

    fix

--- commands/random.js
+++ commands/random.js
@@ -3,23 +3,29 @@ const { JSDOM } = require('jsdom');
 
 exports.run = (client, message) => {
 
+    var cardArray = []; 
+    cardArray.push(getRandomInt(1001,1103).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2001,2038).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2105,2117).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2147,2158).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2184,2194).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2225,2235).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2260,2273).toString().padStart(5, '0'));
+    cardArray.push(getRandomInt(2299,2310).toString().padStart(5, '0'));    
+   
+    num =  cardArray[rand.getRandomInt(cardArray.length)]
+    console.log(num);
+    let linkUrl
+    const http = require('http');
+    linkUrl = 'http://arkhamdb.fr.cr/IMAGES/CARTES/AH-' + num + '.jpg'
+    http.get(linkUrl, (resp) => {
+        const { statusCode } = resp;
+        if (statusCode == 200) {
+            message.reply(linkUrl)
+        
+        } 
+    })
     
-
-    find = false
-    while (find == false){
-        num = getRandomInt(1001,99999).toString().padStart(5, '0');
-        console.log(num);
-        let linkUrl
-        const http = require('http');
-        linkUrl = 'http://arkhamdb.fr.cr/IMAGES/CARTES/AH-' + num + '.jpg'
-        http.get(linkUrl, (resp) => {
-            const { statusCode } = resp;
-            if (statusCode == 200) {
-                message.reply(linkUrl)
-                find = true
-            } 
-        })
-    }
  
 }
 
