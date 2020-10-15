112b9d04d886cf5c69f95f4be98247d369d7d32e
pilunte23 <vad23@msn.com>
2020-10-15 19:04:57 +0200

    complete

--- commands/random.js
+++ commands/random.js
@@ -5,20 +5,41 @@ let rand = require('../function/random.js');
 exports.run = (client, message) => {
 
     var cardArray = []; 
-    addArray(cardArray,1001,1103);
-    addArray(cardArray,2001,2038);
-    addArray(cardArray,2105,2117);
-    addArray(cardArray,2147,2158);
-    addArray(cardArray,2184,2194);
-    addArray(cardArray,2225,2235);
-    addArray(cardArray,2260,2273);
-    addArray(cardArray,2299,2310);    
-    
-    num =  cardArray[rand.getRandomInt(cardArray.length)].toString().padStart(5, '0');
-    console.log(num);
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));
+    cardArray.push(getRandomInt(1001,1103));           
+    cardArray.push(getRandomInt(2001,2038));
+    cardArray.push(getRandomInt(2105,2117));
+    cardArray.push(getRandomInt(2147,2158));
+    cardArray.push(getRandomInt(2184,2194));
+    cardArray.push(getRandomInt(2225,2235));
+    cardArray.push(getRandomInt(2260,2273));
+    cardArray.push(getRandomInt(2299,2310));
+    cardArray.push(getRandomInt(3001,3042));
+    cardArray.push(getRandomInt(3106,3119));
+    cardArray.push(getRandomInt(3147,3158));
+    cardArray.push(getRandomInt(3189,3199));
+    cardArray.push(getRandomInt(3228,3239));
+    cardArray.push(getRandomInt(3263,3273));
+    cardArray.push(getRandomInt(3304,3315));           
+    cardArray.push(getRandomInt(4001,4042));
+    cardArray.push(getRandomInt(4103,4112));
+    cardArray.push(getRandomInt(4149,4160));
+    cardArray.push(getRandomInt(4192,4204));
+    cardArray.push(getRandomInt(4229,4236));
+    cardArray.push(getRandomInt(4265,4276));
+    cardArray.push(getRandomInt(4304,4313));
+   
+    num =  cardArray[rand.getRandomInt(cardArray.length)]
+    numString = num.toString().padStart(5, '0');
+    console.log(numString);
     let linkUrl
     const http = require('http');
-    linkUrl = 'http://arkhamdb.fr.cr/IMAGES/CARTES/AH-' + num + '.jpg'
+    linkUrl = 'http://arkhamdb.fr.cr/IMAGES/CARTES/AH-' + numString + '.jpg'
     http.get(linkUrl, (resp) => {
         const { statusCode } = resp;
         if (statusCode == 200) {
@@ -29,11 +50,12 @@ exports.run = (client, message) => {
     
 }
 
-function addArray( array, min, max) {
-    for (let pas = min ; pas = max; pas++) {
-        array.push(pas)
-      }  
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 exports.help = {
     name: "random"
 };
