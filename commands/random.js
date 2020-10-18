3ffcde2b79deb062c09a5d58bfa46b942c9aaae5
pilunte23 <vad23@msn.com>
2020-10-18 22:17:34 +0200

    random

--- commands/random.js
+++ commands/random.js
@@ -61,6 +61,53 @@ function card(message){
     cardArray.push(getRandomInt(6323,6332));           
  
     num =  cardArray[rand.getRandomInt(cardArray.length)]
+    let pack
+    let campaign
+    switch (true) {
+        case (num < 1104):
+            thumb= "http://arkhamdb.fr.cr/IMAGES/EXT/ext1.png"
+            campaign = "La Nuit de la Zélatrice"
+            pack = "Boite de Base"
+            break;
+        case (num < 2039):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext2.png"
+            campaign = "Cycle de Dunwich"
+            pack = "L'Héritage de Dunwich"
+            break;
+        case (num < 2118):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext6.png"
+            campaign = "Cycle de Dunwich"
+            pack = "Le Musée Miskatonic"
+            break;
+        case (num < 2159):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext8.png"
+            campaign = "Cycle de Dunwich"
+            pack = "L'Express du Comté d'Essex"
+            break;
+        case (num < 2195):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext11.png"
+            campaign = "Cycle de Dunwich"
+            pack = "Du Sang sur l'Autel"
+            break;
+        case (num < 2236):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext12.png"
+            campaign = "Cycle de Dunwich"
+            pack = "Aux Frontières du Visible"
+            break;
+        case (num < 2274):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext13.png"
+            campaign = "Cycle de Dunwich"
+            pack = "Là où le Destin Attend"
+            break;
+        case (num < 2311):
+            thumb="http://arkhamdb.fr.cr/IMAGES/EXT/ext14.png"
+            campaign = "Cycle de Dunwich"
+            pack = "Perdu dans le Temps et l'Espace"
+            break;
+        default:
+            alert("none");
+            break;
+    }
     numString = num.toString().padStart(5, '0');
     console.log(numString);
     let linkUrl
@@ -69,7 +116,16 @@ function card(message){
     http.get(linkUrl, (resp) => {
         const { statusCode } = resp;
         if (statusCode == 200) {     
-            message.reply(linkUrl)
+            //message.reply(linkUrl)
+            const embed = new MessageEmbed()
+            .setTitle("Carte du Jour")
+            .setThumbnail(thumb)
+            .setDescription(campaign)
+            .addField(pack)
+            .setColor("#9B59B6")
+            .setImage(linkUrl)
+            message.reply(embed);
+    
         } 
     })  
 }
