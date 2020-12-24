cc4bccb815c2e70f776cf44235df15e0c34460ab
pilunte23 <vad23@msn.com>
2020-12-24 13:52:18 +0100

    blob

--- commands/blob.js
+++ commands/blob.js
@@ -10,29 +10,14 @@ exports.run = (client, message, args) => {
 
     console.log(args);
 
-    //si le premier argument est numerique c'est la commande de base pour enlever des pv
-    if (isNaN(args[0] )){
-        {
-            if (args[0] == "init"){
-                                    
-            }
-            if (args[0] == "heal"){
+    //channel.send('Le ${channel.name} ajoute ${degat}<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste ${restant}/${pv}')
+    degat = args[0]
+    client.channels.cache.filter(chan => chan.name.startsWith("group") && chan.category === "text").forEach(channel => {
+        console.log(chan.name)
+        channel.send('<msg>')
+        })    
     
-            }
-        } 
-    }
-    else
-    {  
-        //channel.send('Le ${channel.name} ajoute ${degat}<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste ${restant}/${pv}')
-        degat = args[0]
-        client.channels.cache.filter(chan => chan.name.startsWith("group") && chan.category === "text").forEach(channel => {
-            console.log(chan.name)
-            channel.send('<msg>')
-            })    
-    }
 }
-
-
 
 exports.help = {
     name: "helpme"
