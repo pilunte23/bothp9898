41f7c2c61228fa6c8d9105ab236b8a2512097a52
pilunte23 <vad23@msn.com>
2020-12-24 14:10:50 +0100

    blob

--- commands/blob.js
+++ commands/blob.js
@@ -12,9 +12,8 @@ exports.run = (client, message, args) => {
 
     //channel.send('Le ${channel.name} ajoute ${degat}<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste ${restant}/${pv}')
     degat = args[0]
-    console.log(client.channels.cache)
-    client.channels.cache.filter(chan => chan.name.startsWith("group") && chan.category === "text").forEach(channel => {
-        console.log(chan.name)
+    client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {
+        console.log(channel)
         channel.send('<msg>')
         })    
     
