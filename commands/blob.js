3deddef01908b37d8e41a42e22e0668ae872c192
pilunte23 <vad23@msn.com>
2020-12-25 17:51:00 +0100

    blob

--- commands/blob.js
+++ commands/blob.js
@@ -1,4 +1,3 @@
-const { MessageEmbed } = require('discord.js');
 
 totalpv = 0;
 damage = 0;
@@ -6,7 +5,7 @@ contreMesure = 0;
 indice = 0;
 
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 
     console.log(args);
    
@@ -14,23 +13,40 @@ exports.run = (client, message, args) => {
         degat = args[0]
         damage = damage + parseInt(args[0])
         restant = totalpv - damage
-        client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-            channel.send('Le **'+message.channel.name+'** ajoute **'+degat+'**<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste **'+restant+'**/**'+totalpv+'**')
-            }) 
-        
+        SendMessagetoGroup('Le **'+message.channel.name+'** ajoute **'+degat+'**<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste **'+restant+'**/**'+totalpv+'**')         
     }
     if (args[0] == "init"){
         if (!isNaN(args[1])){
             totalpv = 15 * args[1]
             contreMesure = Math.ceil(args[1]/2)
             indice =  2 * args[1]
-            message.channel.send("Total PV <:jelly:733931040942587965> : **"+totalpv+"**\n Total <:TokenClue:443357925369577482> Acte 1 : **"+indice+"**\n Contre mesure : **"+contreMesure+"**")
+            damage = 0
+            SendMessagetoGroup("Total PV <:jelly:733931040942587965> : **"+totalpv+"**\n Total <:TokenClue:443357925369577482> Acte 1 : **"+indice+"**\n Contre mesure : **"+contreMesure+"**")
         }
         else{
             message.channel.send("Il faut mettre le nombre de participant");
         }
     }
-   
+
+    if (args[0] == "cm"){
+        contreMesure = contreMesure--
+        SendMessagetoGroup('\:warning: Le **'+message.channel.name+'** utilise **1 Contre-Mesure** , il en reste **'+contreMesure+'**') 
+    }
+
+    if (args[0] == "i"){
+        if (!isNaN(args[1])){
+            indice  = indice - parseInt(args[1])
+            SendMessagetoGroup('Le **'+message.channel.name+'** depose **'+parseInt(args[1])+' Indice(s)** , il en reste **'+indice+'**') 
+        }else
+        {
+            indice = indice--
+            SendMessagetoGroup('Le **'+message.channel.name+'** depose **1 Indice** , il en reste **'+indice+'**') 
+        }       
+    }  
+}
+
+async function SendMessagetoGroup(text) {
+    await client.channels.cache.filter(async chan => await chan.name.startsWith("group").forEach(async channel => { await channel.send(text)})) 
 }
 
 exports.help = {
