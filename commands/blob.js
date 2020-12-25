abbfdcc01f3be39ea73995bb2c1210439ed2f2f3
pilunte23 <vad23@msn.com>
2020-12-25 18:12:43 +0100

    blob

--- commands/blob.js
+++ commands/blob.js
@@ -1,11 +1,10 @@
-
 totalpv = 0;
 damage = 0;
 contreMesure = 0;
 indice = 0;
 initialIndice = 0;
 
-exports.run = (message, args) => {
+exports.run = (client,message, args) => {
 
     console.log(args);
    
@@ -13,7 +12,7 @@ exports.run = (message, args) => {
         degat = args[0]
         damage = damage + parseInt(args[0])
         restant = totalpv - damage
-        SendMessagetoGroup('Le **'+message.channel.name+'** ajoute **'+degat+'**<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste **'+restant+'**/**'+totalpv+'**')         
+        SendMessagetoGroup(client,'Le **'+message.channel.name+'** ajoute **'+degat+'**<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste **'+restant+'**/**'+totalpv+'**')         
     }
     if (args[0] == "init"){
         if (!isNaN(args[1])){
@@ -25,35 +24,35 @@ exports.run = (message, args) => {
             SendMessagetoGroup("Total PV <:jelly:733931040942587965> : **"+totalpv+"**\n Total <:TokenClue:443357925369577482> Acte 1 : **"+indice+"**\n Contre mesure : **"+contreMesure+"**")
         }
         else{
-            message.channel.send("Il faut mettre le nombre de participant");
+            message.channel.send(client,"Il faut mettre le nombre de participant");
         }
     }
 
     if (args[0] == "cm"){
         contreMesure = contreMesure--
-        SendMessagetoGroup('\:warning: Le **'+message.channel.name+'** utilise **1 Contre-Mesure** , il en reste **'+contreMesure+'**') 
+        SendMessagetoGroup(client,'\:warning: Le **'+message.channel.name+'** utilise **1 Contre-Mesure** , il en reste **'+contreMesure+'**') 
     }
 
     if (args[0] == "i"){
         if (!isNaN(args[1])){
             indice  = indice - parseInt(args[1])
-            SendMessagetoGroup('Le **'+message.channel.name+'** depose **'+parseInt(args[1])+' Indice(s)** , il en reste **'+indice+'**') 
+            SendMessagetoGroup(client,'Le **'+message.channel.name+'** depose **'+parseInt(args[1])+' Indice(s)** , il en reste **'+indice+'**') 
         }else
         {
             indice = indice--
-            SendMessagetoGroup('Le **'+message.channel.name+'** depose **1 Indice** , il en reste **'+indice+'**') 
+            SendMessagetoGroup(client,'Le **'+message.channel.name+'** depose **1 Indice** , il en reste **'+indice+'**') 
         }       
     }
     if (args[0] == "reset"){
         if (!isNaN(args[1])){
             indice  = initialIndice
-            SendMessagetoGroup('Reinitialisation du nombre d indice Acte 1 à **'+indice+'**') 
+            SendMessagetoGroup(client,'Reinitialisation du nombre d indice Acte 1 à **'+indice+'**') 
         }  
     }    
 }
 
-function SendMessagetoGroup(text) {
-    client.channels.cache.filter(chan => chan.name.startsWith("group").forEach(channel => { channel.send(text)})) 
+function SendMessagetoGroup(myclient,text) {
+    myclient.channels.cache.filter(chan => chan.name.startsWith("group").forEach(channel => { channel.send(text)})) 
 }
 
 exports.help = {
