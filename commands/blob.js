1e180e47c6914ebbefbad2008e9cb54b62cb8d5f
pilunte23 <vad23@msn.com>
2020-12-26 13:20:34 +0100

    maj

--- commands/blob.js
+++ commands/blob.js
@@ -10,71 +10,56 @@ initialIndice = 0;
 exports.run = (client, message, args) => {
 
     console.log(args);
-   
+    //Command for players
     if (message.channel.name.startsWith("group") && !isNaN(args[0])){
         degat = args[0]
         damage = damage + parseInt(args[0])
         restant = totalpv - damage
-        if (restant < 0){
+        if (restant > 0){
             SendMessage(client,'Le **'+message.channel.name+'** ajoute **'+degat+'**<:TokenDamage:443355098773585920> sur <:jelly:733931040942587965> : il lui reste **'+restant+'**/**'+totalpv+'**')       
         }else
         {
-            SendMessage(client,'**Félicitation** les \:spy: ont vaincu \:skill_crossbones:<:jelly:733931040942587965>\:skill_crossbones:')       
-        }
-        
+            SendMessage(client,'**Félicitation** les \:spy: ont vaincu \:skull_crossbones:<:jelly:733931040942587965>\:skull_crossbones:')       
+        }    
     }
-
-    if (args[0] == "init"){
-        if (!isNaN(args[1])){
-            totalpv = 15 * args[1]
-            contreMesure = Math.ceil(args[1]/2)
-            indice =  2 * args[1]
-            initialIndice = indice
-            damage = 0
-            message.channel.send("Total PV <:jelly:733931040942587965> : **"+totalpv+"**\n Total <:TokenClue:443357925369577482> Acte 1 : **"+indice+"**\n Contre mesure : **"+contreMesure+"**")
-        }
-        else{
-            message.channel.send(client,"Il faut mettre le nombre de participant");
-        }
-    }
-   
+ 
     if (args[0] == "cm"){
         if (args[1] == "+"){
             contreMesure = contreMesure + 1
-            client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-                channel.send('\:ok_hand: Bonne nouvelle, Le **'+message.channel.name+'** ajoute **1 Contre-Mesure** , il en reste **'+contreMesure+'**')
-            })
+            SendMessage(client,'\:ok_hand: Bonne nouvelle, Le **'+message.channel.name+'** ajoute **1 Contre-Mesure** , il en reste **'+contreMesure+'**')
         }
         if (args[1] == "-" || args[1] == null){
-            contreMesure = contreMesure - 1
-            client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-                channel.send('\:warning: Le **'+message.channel.name+'** utilise **1 Contre-Mesure** , il en reste **'+contreMesure+'**')
-            })
+            if (contreMesure > 0){
+                contreMesure = contreMesure - 1
+                SendMessage(client,'\:warning: Le **'+message.channel.name+'** utilise **1 Contre-Mesure** , il en reste **'+contreMesure+'**')
+            }else
+            {
+                message.channel.send("Désolé les \:spy: , vous n'avez plus de **Contre-Mesure**; il en reste 0");
+            }
         }
-        
-
     }
 
     if (args[0] == "i" || args[0] == "indice"){
         if (!isNaN(args[1])){
             indice  = indice - parseInt(args[1])
-            client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-                channel.send('Le **'+message.channel.name+'** depose **'+parseInt(args[1])+'<:TokenClue:443357925369577482>** , il en reste **'+indice+'**<:TokenClue:443357925369577482> à trouver')
-            })
+            if (indice > 0){
+                SendMessage(client,'Le **'+message.channel.name+'** depose **'+parseInt(args[1])+'<:TokenClue:443357925369577482>** , il en reste **'+indice+'**<:TokenClue:443357925369577482> à trouver')       
+            }else
+            {
+                SendMessage(client,'**Félicitation** les \:spy: ont découvert la totalité des <:TokenClue:443357925369577482>')
+            }
+           
         }else
         {
             indice = indice - 1
-            client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-                channel.send('Le **'+message.channel.name+'** depose **1 <:TokenClue:443357925369577482>** , il en reste **'+indice+'**<:TokenClue:443357925369577482> à trouver')
-            })
+            if (indice > 0){
+                SendMessage(client,'Le **'+message.channel.name+'** depose **1 <:TokenClue:443357925369577482>** , il en reste **'+indice+'**<:TokenClue:443357925369577482> à trouver')
+            }else
+            {
+                SendMessage(client,'**Félicitation** les \:spy: ont découvert la totalité des <:TokenClue:443357925369577482>')
+            }
+                  
         }       
-    }
-
-    if (args[0] == "reset"){
-        indice  = initialIndice
-        client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-            channel.send('Reinitialisation Acte 1 à **'+indice+'<:TokenClue:443357925369577482>**')
-            })
     }
 
     if (args[0] == "help" || args[0] == "aide" ){
@@ -92,7 +77,8 @@ exports.run = (client, message, args) => {
             .addField("!blob eat", "Permet de savoir quel aspect de la réalité est dévoré (a faire peut etre)")
             message.channel.send(embed);
     }
-    if (args[0] == "welcome"){
+    //Command for admin
+    if (args[0] == "welcome" && message.channel.name == "Admin-Event"){
         let embed = new MessageEmbed()
             .setTitle("**Dévoreur de Toute Chose**")
             .setColor("#67C355")
@@ -100,25 +86,43 @@ exports.run = (client, message, args) => {
             .setImage('attachment://green.png')
             .addField("Toutes les commandes pour l'evenement commencent par !blob.", "Le !b peut être utilisé en raccourci")
             .addField("!blob help ou !blob aide", "Pour obtenir la liste des commandes à tout moment")
-            client.channels.cache.filter(chan => chan.name.startsWith("group")).forEach(channel => {          
-                channel.send(embed)
-            })
+            SendMessage(client,embed)
     }
-    if (args[0] == "admin"){
+    
+    if (args[0] == "admin" && message.channel.name == "Admin-Event"){
         let embed = new MessageEmbed()
             .setTitle("Administration Dévoreur de Toute Chose")
             .attachFiles(imgJelly)
             .setThumbnail('attachment://jelly.png')
             .setColor("#67C355")
             .addField("!blob init suivi d'un chiffre ", "Initialistions des compteurs selon le nombre de participants")
+            .addField("!blob timer ", "Lance le timer si pas de chiffre indiqué alors 180minutes (a faire)")
             .addField("!blob reset", "Reinitialise les indices de l'acte 1")
             .addField("!blob story", "Selectionne aléatoirement et Annonce l'histoire selectionné à chaque groupe (a faire)")
             .addField("!blob fixPV suivi d'un chiffre", "Refixe le nombre de PV suite missplay ou crashbot (a faire)")
             .addField("!blob fixI suivi d'un chiffre", "Refixe le nombre d'indice suite missplay ou crashbot (a faire)")
             .addField("!blob fixCM suivi d'un chiffre", "Refixe le nombre de contre mesure suite missplay ou crashbot (a faire)")
-            message.author.send(embed);
+            message.channel.send(embed);
     }
 
+    if (args[0] == "init" && message.channel.name == "Admin-Event"){
+        if (!isNaN(args[1])){
+            totalpv = 15 * args[1]
+            contreMesure = Math.ceil(args[1]/2)
+            indice =  2 * args[1]
+            initialIndice = indice
+            damage = 0
+            SendMessage(client,"Total PV <:jelly:733931040942587965> : **"+totalpv+"**\n Total <:TokenClue:443357925369577482> Acte 1 : **"+indice+"**\n Contre mesure : **"+contreMesure+"**")
+        }
+        else{
+            message.channel.send(client,"Il faut mettre le nombre de participant");
+        }
+    }
+
+    if (args[0] == "reset" && message.channel.name == "Admin-Event"){
+        indice  = initialIndice
+        SendMessage(client,'Reinitialisation Acte 1 à **'+indice+'<:TokenClue:443357925369577482>**')
+    }
 }
 
 function SendMessage(client,messagetoGroup){
